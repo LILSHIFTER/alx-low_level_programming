8fc4513b79697de106eaf2b87ca464d5d9e81a44
LILSHIFTER <fhamr2014@gmail.com>
2023-04-27 17:35:34 -0700

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,41 +1,52 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 
+int _strlen_recursion(char *s);
+
 /**
- * add_node_end - adds a new node at the end of a linked list
- * @head: double pointer to the list_t list
- * @str: string to put in the new node
- *
+ * add_node_end - adds a new node at the end of a list_t list.
+ * @head: pointer to the first element of the list.
+ * @str: string to set in the new node.
  * Return: address of the new element, or NULL if it failed
- */
+ **/
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
-	list_t *temp = *head;
-	unsigned int len = 0;
-
-	while (str[len])
-		len++;
+	list_t *new, *aux = *head;
 
 	new = malloc(sizeof(list_t));
-	if (!new)
+	if (new == NULL)
+	{
 		return (NULL);
-
+	}
 	new->str = strdup(str);
-	new->len = len;
+	if (!new->str)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = _strlen_recursion(new->str);
 	new->next = NULL;
 
-	if (*head == NULL)
+	if (aux)
 	{
-		*head = new;
-		return (new);
+		while (aux->next)
+			aux = aux->next;
+		aux->next = new;
 	}
-
-	while (temp->next)
-		temp = temp->next;
-
-	temp->next = new;
+	else
+		*head = new;
 
 	return (new);
+}
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string.
+ * Return: length of @s.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
